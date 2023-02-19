72da3f37b533c0066d2b2c18834fbfd11ba98a81
Tobias <Tobiashague@gmail.com>
2023-02-19 18:02:36 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,112 @@
+var title = document.getElementById("titlemain");
+var menu = document.getElementById("menu");
+
+var Aunt_Maggy = document.getElementById("Aunt_Maggy");
+var c_maggy_vid = document.getElementById("c_maggy_vid");
+var d_maggy_vid = document.getElementById("d_maggy_vid");
+
+var Blackpool_Rock = document.getElementById("Blackpool_Rock");
+var c_blackpool = document.getElementById("c_blackpool_vid");
+var d_blackpool = document.getElementById("d_blackpool_vid");
+
+var Chinese_Laundry = document.getElementById("Chinese_Laundry");
+var c_chinese = document.getElementById("c_chinese_vid");
+var d_chinese = document.getElementById("d_chinese_vid");
+
+var Fanlight_Fanny = document.getElementById("Fanlight_Fanny");
+var c_fanlight = document.getElementById("c_fanlight_vid");
+var d_fanlight = document.getElementById("d_fanlight_vid");
+
+var Grandads_Flannelette = document.getElementById("Grandads_Flannelette");
+var d_grandad = document.getElementById("c_grandad_vid");
+var d_grandad = document.getElementById("c_grandad_vid");
+
+var Leaning_On_A_Lamp_Post = document.getElementById("Leaning_On_A_Lamp_Post");
+var c_lamp = document.getElementById("c_lamp_vid");
+var d_lamp = document.getElementById("d_lamp_vid");
+
+var On_The_Wigan = document.getElementById("On_The_Wigan");
+var c_wigan = document.getElementById("c_wigan_vid");
+var d_wigan = document.getElementById("d_wigan_vid");
+
+var Our_Sergeant = document.getElementById("Our_Sergeant");
+var c_sergeant = document.getElementById("c_sergeant_vid");
+var d_sergeant = document.getElementById("d_sergeant_vid");
+
+var When_Im_Cleaning_Windows = document.getElementById("When_Im_Cleaning_Windows");
+var c_windows = document.getElementById("c_windows_vid");
+var d_windows = document.getElementById("d_windows_vid");
+
+var With_My_Little_Ukelele = document.getElementById("With_My_Little_Ukelele");
+var c_ukelele = document.getElementById("c_ukelele_vid");
+var d_ukelele = document.getElementById("d_ukelele_vid");
+
+
+function aunt() {
+    menu.style.display ="none";
+    Aunt_Maggy.style.display ="grid";
+}
+
+function blackpool() {
+    menu.style.display ="none";
+    Blackpool_Rock.style.display ="grid";
+}
+
+function chinese() {
+    menu.style.display ="none";
+    Chinese_Laundry.style.display ="grid";
+}
+
+function fanlight() {
+    menu.style.display ="none";
+    Fanlight_Fanny.style.display ="grid";
+}
+
+function lamp() {
+    menu.style.display ="none";
+    Leaning_On_A_Lamp_Post.style.display ="grid";
+}
+
+function grandad() {
+    menu.style.display ="none";
+    Grandads_Flannelette.style.display ="grid";
+}
+
+function wingan() {
+    menu.style.display ="none";
+    On_The_Wigan.style.display ="grid";
+}
+
+function sergeant() {
+    menu.style.display ="none";
+    Our_Sergeant.style.display ="grid";
+}
+
+function windows() {
+    menu.style.display ="none";
+    When_Im_Cleaning_Windows.style.display ="grid";
+}
+
+function ukelele() {
+    menu.style.display ="none";
+    With_My_Little_Ukelele.style.display ="grid";
+}
+
+
+
+
+
+function back() {
+    menu.style.display ="block";
+    Aunt_Maggy.style.display ="none";
+    Blackpool_Rock.style.display ="none";
+    Chinese_Laundry.style.display ="none";
+    Fanlight_Fanny.style.display ="none";
+    Grandads_Flannelette.style.display ="none";
+    Leaning_On_A_Lamp_Post.style.display ="none";
+    On_The_Wigan.style.display ="none";
+    Our_Sergeant.style.display ="none";
+    When_Im_Cleaning_Windows.style.display ="none";
+    With_My_Little_Ukelele.style.display ="none";
+}
+
